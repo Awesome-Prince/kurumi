b83e0e38813fda55157fe016daaaa83be1102b97
Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2021-10-04 17:17:51 +0530

    Update __main__.py

--- KURUMIBOT/__main__.py
+++ KURUMIBOT/__main__.py
@@ -534,7 +534,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}"💕Iam Busy With My Boyfriend😘 @Horimaya 🤭, "[!](https://telegra.ph/file/97f8731118bc43fd96086.mp4)", parse_mode=ParseMode.MARKDOWN) 
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "[I Am Busy With My Boy Friend 😘🤭](https://telegra.ph/file/97f8731118bc43fd96086.mp4)", parse_mode=ParseMode.MARKDOWN) 
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!")
